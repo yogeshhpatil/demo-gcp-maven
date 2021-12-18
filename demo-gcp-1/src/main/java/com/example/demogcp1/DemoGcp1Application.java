dca5ccbb26c1aff5e91142a2eb7d215b9bc3173d
Yogesh Patil <yogesh.hpatil4@gmail.com>
2021-12-18 15:20:37 +0530

    Initial commit

---
+++ demo-gcp-1/src/main/java/com/example/demogcp1/DemoGcp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demogcp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGcp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGcp1Application.class, args);
+	}
+
+}
---
+++ demo-gcp-1/src/test/java/com/example/demogcp1/DemoGcp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogcp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGcp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
